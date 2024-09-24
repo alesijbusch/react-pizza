5c8a9aa65ac9877e9f233ac152022e1478d0bf92
Alesia <alesijbusch@gmail.com>
2024-09-24 22:21:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Sort from "./components/sort";
+import Categories from "./components/Categories";
+import PizzaBlock from "./components/PizzaBlock";
+import "./scss/app.scss";
+import pizzas from "./assets/block-pizzas.json";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              {pizzas.map((obj) => (
+                <PizzaBlock key={obj.id} {...obj} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Categories = () => {
+  const [activIndex, setActiveIndex] = useState(0);
+
+  const handelCategory = (index) => {
+    setActiveIndex(index);
+  };
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => (
+          <li
+            onClick={() => handelCategory(i)}
+            className={activIndex === i ? "active" : ""}
+            key={value}
+          >
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
